af555dd0ad7a2f28ccf4daa1338dcf777ccac5e1
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2024-01-09 23:02:55 +0100

    components are splitted

---
+++ src/WatchedMovieList.jsx
@@ -0,0 +1,13 @@
+import WatchedMovie from "./WatchedMovie";
+
+function WatchedMovieList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+export default WatchedMovieList;
